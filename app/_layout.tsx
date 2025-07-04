1638eb5e1e149dc1a918e7d5ef6d77b07bdc2808
spike04 <rubinbajracharya@yahoo.com>
2025-07-04 23:20:59 +0545

    chor: assets and fonts loaded

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,24 @@
-import { Stack } from 'expo-router'
+import { useFonts } from 'expo-font'
+import { SplashScreen, Stack } from 'expo-router'
+import { useEffect } from 'react'
 import './globals.css'
 
 export default function RootLayout() {
-  return <Stack />
+  const [fontsLoaded, error] = useFonts({
+    'QuickSand-Regular': require('../assets/fonts/Quicksand-Regular.ttf'),
+    'QuickSand-Bold': require('../assets/fonts/Quicksand-Bold.ttf'),
+    'QuickSand-SemiBold': require('../assets/fonts/Quicksand-SemiBold.ttf'),
+    'QuickSand-Light': require('../assets/fonts/Quicksand-Light.ttf'),
+    'QuickSand-Medium': require('../assets/fonts/Quicksand-Medium.ttf'),
+  })
+
+  useEffect(() => {
+    if (error) throw error
+
+    if (fontsLoaded) {
+      SplashScreen.hideAsync()
+    }
+  }, [fontsLoaded, error])
+
+  return <Stack screenOptions={{ headerShown: false }} />
 }
--- app/index.tsx
+++ app/index.tsx
@@ -3,8 +3,8 @@ import { Text, View } from 'react-native'
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
+      <Text className="text-5xl font-bold text-primary text-center font-quicksand-bold">
+        Welcome to my React Native App!
       </Text>
     </View>
   )
