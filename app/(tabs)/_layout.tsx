9496e5cabd6424bd375b95449af47c44209847ed
spike04 <rubinbajracharya@yahoo.com>
2025-07-07 14:55:38 +0545

    chor: routing and essential screens

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,11 @@
+import { Redirect, Slot } from 'expo-router'
+import React from 'react'
+
+export default function TabLayout() {
+  const isAuthenticated = false
+
+  if (!isAuthenticated) {
+    return <Redirect href="/sign-in" />
+  }
+  return <Slot />
+}
