bd0b38be732579061e13d7d4792a42a7714d8c76
spike04 <rubinbajracharya@yahoo.com>
2025-07-07 15:04:05 +0545

    chor: appwrite setup

---
+++ lib/appwrite.ts
@@ -0,0 +1,5 @@
+export const appwriteConfig = {
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  platform: 'com.rubin.foodordering',
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+}
