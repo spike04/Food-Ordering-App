8b68064dfca95f5bae87e1e496707293529a88b1
spike04 <rubinbajracharya@yahoo.com>
2025-07-14 10:14:48 +0545

    chor: implement tab navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,28 @@
+import { images } from '@/constants'
 import useAuthStore from '@/store/auth.store'
-import { Redirect, Slot } from 'expo-router'
-import React from 'react'
+import { TabBarIconProps } from '@/types'
+import cn from 'clsx'
+import { Redirect, Tabs } from 'expo-router'
+import { Image, Text, View } from 'react-native'
+
+const TabBarIcon = ({ focused, icon, title }: TabBarIconProps) => (
+  <View className="tab-icon">
+    <Image
+      source={icon}
+      className="size-7"
+      resizeMode="contain"
+      tintColor={focused ? '#FE8C00' : '#5D5F6D'}
+    />
+    <Text
+      className={cn(
+        'text-sm font-bold',
+        focused ? 'text-primary' : 'text-gray-200',
+      )}
+    >
+      {title}
+    </Text>
+  </View>
+)
 
 export default function TabLayout() {
   const { isAuthenticated } = useAuthStore()
@@ -8,5 +30,66 @@ export default function TabLayout() {
   if (!isAuthenticated) {
     return <Redirect href="/sign-in" />
   }
-  return <Slot />
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          borderRadius: 50,
+          height: 80,
+          marginHorizontal: 20,
+          position: 'absolute',
+          bottom: 24,
+          backgroundColor: '#fff',
+          shadowColor: '#1a1a1a',
+          shadowOffset: { width: 0, height: 2 },
+          shadowOpacity: 0.1,
+          shadowRadius: 4,
+          elevation: 5,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon focused={focused} icon={images.home} title="Home" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: 'Search',
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon focused={focused} icon={images.search} title="Search" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="cart"
+        options={{
+          title: 'Cart',
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon focused={focused} icon={images.bag} title="Cart" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profile',
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon
+              focused={focused}
+              icon={images.person}
+              title="Profile"
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  )
 }
